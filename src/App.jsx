845e305192b77a6ca725447a95d9c3095baed1d4
Tarun Mandhan <mandhantarun@gmail.com>
2022-12-21 09:44:21 +0530

    add components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import CreateNote from "./CreateNote";
+import Note from "./Note";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <CreateNote />
+      <Note />
+    </>
+  );
+};
+
+export default App;
