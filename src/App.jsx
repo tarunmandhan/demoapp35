1600b930b5914132b05bfa00a3efb197bc4c3869
Tarun Mandhan <mandhantarun@gmail.com>
2022-12-21 18:07:05 +0530

    identify data name ->store in setAddItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import CreateNote from "./CreateNote";
 import Note from "./Note";
 
 const App = () => {
+  const [addItem, setAddItem] = useState([]);
+  const addNote = (note) => {
+    //alert("I am clicked");
+    setAddItem((prevData) => {
+      return [...prevData, note];
+    });
+    // console.log(note);
+  };
+
   return (
     <>
       <Header />
+      <CreateNote passNote={addNote} />
+      {/* <Note /> */}
+      {addItem.map((val, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={val.title}
+            content={val.content}
+          />
+        );
+      })}
       <Footer />
-      <CreateNote />
-      <Note />
     </>
   );
 };
